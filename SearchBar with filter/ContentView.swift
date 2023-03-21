5b5168d48f0078660e10eefb7ed10c5f1226bfa0
KarinaKR00 <kr_karina00@mail.ru>
2023-03-21 12:11:28 +0300

    Initial Commit

---
+++ SearchBar with filter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SearchBar with filter
+//
+//  Created by Карина on 21.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchBar with filter/SearchBar_with_filterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBar_with_filterApp.swift
+//  SearchBar with filter
+//
+//  Created by Карина on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchBar_with_filterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
